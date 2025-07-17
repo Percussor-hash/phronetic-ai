b7a8e46d7b4d2cc10375fe72308d32079abaf30b
Percussor-hash <85029612+Percussor-hash@users.noreply.github.com>
2025-07-17 23:11:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,5 +71,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # ✅ Makes Render use its port
+    port = int(os.environ.get("PORT", 5000))  # Makes Render use its port
     app.run(host='0.0.0.0', port=port)
